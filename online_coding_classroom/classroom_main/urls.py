2608d8123142bab2c3f774e04defd03301d6df5f
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-11-04 10:14:20 +0000

    Added login view

--- online_coding_classroom/classroom_main/urls.py
+++ online_coding_classroom/classroom_main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="classroom-home"),
+    path('login', views.login, name="classroom-login"),
     path('createaccount', views.create_account, name="classroom-create-account"),
     path('myaccount', views.my_account, name="classroom-my-account"),
     path('performance-analysis', views.performance_analysis, name="classroom-performance-analysis"),
--- online_coding_classroom/classroom_main/views.py
+++ online_coding_classroom/classroom_main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-# Create your views here.
 def home(request):
     """ View for mainsite homepage """
     context = {}
     return render(request, 'classroom_main/home.html', context)
+    
+def login(request):
+    """ View for login path """
+    context = {}
+    return render(request, 'classroom_main/login.html', context)
 
 def create_account(request):
     """ View for Create Account page """
