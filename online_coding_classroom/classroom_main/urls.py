80ee2006feeb5ea62c303a328617b28ec31a61e9
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-11-04 10:01:01 +0000

    Added view paths to url patterns

--- online_coding_classroom/classroom_main/urls.py
+++ online_coding_classroom/classroom_main/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="classroom-home"),
     path('createaccount', views.create_account, name="classroom-create-account"),
+    path('myaccount', views.my_account, name="classroom-my-account"),
+    path('performance-analysis', views.performance_analysis, name="classroom-performance-analysis"),
+    path('select-language', views.select_language, name="classroom-select-language"),
+    path('select-lesson', views.select_lesson, name="classroom-select-lesson"),
 ]
