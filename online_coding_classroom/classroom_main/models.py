e02fd63841324101fa47a4646c306df58e12e40d
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-11-04 16:20:43 +0000

    Started creation of Class model

--- online_coding_classroom/classroom_main/models.py
+++ online_coding_classroom/classroom_main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # Create your models here.
+class Class(models.Model):
+    """ Model for classes """
+    classNum = models.TextField(max_length=15)
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    students = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    # MAY NEED ADMINMODEL INLINE FOR THIS
+
+    def __str__(self):
+        return self.classNum
