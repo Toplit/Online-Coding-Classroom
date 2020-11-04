cd66e8fdcae1f6b3e01d037a597eca4c1a528d44
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-11-04 14:45:04 +0000

    Created User app, User model and User manager for creating users

---
+++ online_coding_classroom/user/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+ROLE_CHOICES = (
+    ('TEACHER', 'Teacher'),
+    ('STUDENT', 'Student'),
+    ('AVERAGE', 'Average User'),
+)
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, username, role, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("Users must have an email")
+        if not username:
+            raise ValueError("Users must have a username")
+        if not role:
+            raise ValueError("Users must have a role")
+        if not first_name:
+            raise ValueError("Users must have a first name")
+        if not last_name:
+            raise ValueError("Users must have a last name")
+        user = self.model(
+            email=self.normalize_email(email),
+            username = username,
+            role = role,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, role, first_name, last_name, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username = username,
+            role = role,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    """ Custom User model """
+
+    email        = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username     = models.CharField(max_length=30, unique=True)
+    role         = models.CharField(choices=ROLE_CHOICES)
+    first_name   = models.CharField(max_length=25)
+    last_name    = models.CharField(max_length=30)
+    # ADD AWARDS ONCE AWARDS COLLECTION IS CREATED
+    date_joined  = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login   = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin     = models.BooleanField(default=False)
+    is_active    = models.BooleanField(default=True)
+    is_staff     = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'role', 'first_name', 'last_name',]
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """ Function for checking admin permission """
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        """ Function for checking module permissions """
+        return True
