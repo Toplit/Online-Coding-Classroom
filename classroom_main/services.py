102a766248b8f3fe86684a503da39afce4e906d5
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2021-03-31 12:50:56 +0100

    Modified code to prevent academic functions | These will be added back in later

--- classroom_main/services.py
+++ classroom_main/services.py
@@ -1,10 +1,22 @@
 from django.contrib.auth import get_user_model
 
-def createNewUser(form):
+def createNewAcademicUser(form):
     # Retrieve and clean data from each form field
     email = form.cleaned_data.get('email')
     username = form.cleaned_data.get('username')
     role = form.cleaned_data.get('role')
+    first_name = form.cleaned_data.get('first_name')
+    last_name = form.cleaned_data.get('last_name')
+    password = form.cleaned_data.get('password1')
+
+    # Create the user and save it to the database
+    get_user_model().objects.create_user(email, username, role, first_name, last_name, password)
+
+def createNewUser(form):
+    # Retrieve and clean data from each form field
+    email = form.cleaned_data.get('email')
+    username = form.cleaned_data.get('username')
+    role = "AVERAGE"
     first_name = form.cleaned_data.get('first_name')
     last_name = form.cleaned_data.get('last_name')
     password = form.cleaned_data.get('password1')
--- classroom_main/tests/test_services.py
+++ classroom_main/tests/test_services.py
@@ -1,15 +1,22 @@
 from django.test import TestCase, Client
 from classroom_main import services
-from user.forms import AvgRegisterForm
+from user.forms import AvgRegisterForm, AcademicRegisterForm
 from django.contrib.auth import get_user_model
 from user.models import User
 
 class TestServices(TestCase):
     """ Test suite for 'classroom_main' services """
     def setUp(self):
-        self.form = AvgRegisterForm(data={'email' : 'Testing@Email.com',
+        self.formAvg = AvgRegisterForm(data={'email' : 'Testing@Email.com',
                                           'username' : 'Tester',
-                                          'role' : 'AVERAGE',
+                                          'first_name' : 'TesterName',
+                                          'last_name' : 'TesterSurname',
+                                          'password1' : 'TestPassword',
+                                          'password2' : 'TestPassword'})
+
+        self.formAcademic = AcademicRegisterForm(data={'email' : 'Testing2@Email.com',
+                                          'username' : 'Tester2',
+                                          'role' : 'TEACHER',
                                           'first_name' : 'TesterName',
                                           'last_name' : 'TesterSurname',
                                           'password1' : 'TestPassword',
@@ -18,9 +25,22 @@ class TestServices(TestCase):
     def test_createNewUser(self):
         """ Method for testing the createNewUser method """
         # form.is_valid() must be run to give the form a 'cleaned_data' attribute
-        if(self.form.is_valid()): 
-            services.createNewUser(self.form)
+        if(self.formAvg.is_valid()): 
+            services.createNewUser(self.formAvg)
         newUser = get_user_model().objects.filter(username__iexact="Tester")
         self.assertEqual(newUser[0].username, "Tester")
+        self.assertEqual(newUser[0].role, "AVERAGE")
+        self.assertFalse(newUser[0].is_staff)
+        self.assertIsInstance(newUser[0], User)
+
+    def test_createNewAcademicUser(self):
+        """ Method for testing the createNewUser method """
+        # form.is_valid() must be run to give the form a 'cleaned_data' attribute
+        if(self.formAcademic.is_valid()): 
+            services.createNewAcademicUser(self.formAcademic)
+        newUser = get_user_model().objects.filter(username__iexact="Tester2")
+        self.assertEqual(newUser[0].username, "Tester2")
+        self.assertEqual(newUser[0].role, "TEACHER")
+        self.assertTrue(newUser[0].is_staff)
         self.assertIsInstance(newUser[0], User)
         
