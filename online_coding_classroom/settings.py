118f9e2ecd74bbffb0b127e9cf8f6d680dc680de
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2021-04-08 11:32:57 +0100

    Added forgotten password/password reset functionality

--- online_coding_classroom/settings.py
+++ online_coding_classroom/settings.py
@@ -172,9 +172,16 @@ USE_TZ = True
 
 
 
-
-# Email settings for console backend email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if 'test' in sys.argv:
+    # Email settings for console backend email
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
 
--- online_coding_classroom/urls.py
+++ online_coding_classroom/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
     path('', include('classroom_main.urls')),   # Includes the URLS defined in classroom_main.urls
     path('', include('lesson.urls')),           # Includes the URLS defined in lesson.urls
     path('login/', auth_views.LoginView.as_view(template_name='classroom_main/login.html'), name="login"),    # Use the login.html as a LoginView class view 
-    path('logout/', auth_views.LogoutView.as_view(template_name='classroom_main/logout.html'), name="logout") # Use the logout.html as a LogoutView class view 
+    path('logout/', auth_views.LogoutView.as_view(template_name='classroom_main/logout.html'), name="logout"), # Use the logout.html as a LogoutView class view 
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='classroom_main/password_reset.html'), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='classroom_main/password_reset_done.html'), name="password_reset_done"),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='classroom_main/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='classroom_main/password_reset_complete.html'), name="password_reset_complete"),
 ]
